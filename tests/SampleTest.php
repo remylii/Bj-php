862c4c612eaa740eeeb353cf682e141914617d03
remyli <rapi.remyli@gmail.com>
2019-05-27 19:15:33 +0900

    PHPUnit init

---
+++ tests/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SampleTest extends TestCase
+{
+    public function testHello(): void
+    {
+        $this->assertTrue(true);
+    }
+}
+
